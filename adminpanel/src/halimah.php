c19932e83e050bda4b471de22fa301f4041567b0
OniAdekunle <40856525+OniAdekunle@users.noreply.github.com>
2021-08-25 12:02:30 +0100

    Update halimah.php

--- adminpanel/src/halimah.php
+++ adminpanel/src/halimah.php
@@ -1,9 +1,15 @@
 <?php
 
-$Hamzathostname_Mirror = "your server name here";
-$HamzatUser_Mirrior = "your username here";
-$HamzatPass_Mirror = "your password here";
-$Hamzatdatabase_Mirrior = "your database name here";
+//$Hamzathostname_Mirror = "your server name here";
+//$HamzatUser_Mirrior = "your username here";
+//$HamzatPass_Mirror = "your password here";
+//$Hamzatdatabase_Mirrior = "your database name here";
+
+$Hamzathostname_Mirror = "remotemysql.com";
+$HamzatUser_Mirrior = "cOYfI99iLm";
+$HamzatPass_Mirror = "03GcT6IPsK";
+$Hamzatdatabase_Mirrior = "cOYfI99iLm";
+
 
 $db = mysqli_connect($Hamzathostname_Mirror, $HamzatUser_Mirrior, $HamzatPass_Mirror, $Hamzatdatabase_Mirrior); 
 if(mysqli_connect_errno()) {
